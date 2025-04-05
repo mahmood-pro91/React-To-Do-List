be640626d1578b32b5528958bc024b9f3a61a35b
promahmorta <promahmorta@gmail.com>
2025-04-05 19:31:13 +0200

    add , modefied ,del item in the list

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setToDo] = useState([])
 
+  let text = "to do list"
+
+  const inputRef = useRef()
+  // add item for list
+  const handleAddtodo = () => {
+    const textTodo = inputRef.current.value;
+    const newItem = {complete:false,textTodo}
+    setToDo([...todo,newItem])
+    inputRef.current.value =""
+  }
+  // change complete and done item
+  const handleDone = (index)=>{
+    const newTodo =[...todo]
+    newTodo[index].complete = !newTodo[index].complete
+    setToDo(newTodo)
+
+  }
+  //delete current item
+  const handleDel =(index) =>{
+    const newTodo =[...todo]
+    newTodo.splice(index,1)
+    setToDo(newTodo)
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='app'>
+        <h2 className='header'>{text.toUpperCase()}</h2>
+        <ul className='list'>
+
+          {todo.map(({complete,textTodo},index) => {
+            return (
+              <div className='item-contanier'>
+                <li onClick={() => handleDone(index)} 
+                  key={index}  className={complete ? "done":""} >
+                  {textTodo}
+                </li>
+                <span onClick={() =>handleDel(index) }>✖️</span>
+              </div>
+                )
+          })}
+        </ul>
+        <label htmlFor="added">Add To Task List :-  
+          <input ref={inputRef} id='added' name='added' placeholder='Add your Task' type="text" />
+        </label>
+        <button onClick={handleAddtodo} value="Add">Add</button>
+      </div>    
     </>
   )
 }
